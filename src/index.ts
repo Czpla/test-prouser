f99885f658eae0e1ab0d55dd37802a9816705071
Eduardo Czpla <edu.czpla@gmail.com>
2022-04-01 01:11:22 -0300

    add index.ts for call

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world');
+import data from './file/ips.json';
+import { SeparatedData } from './utils/separated-data';
+
+const separatedData = new SeparatedData(data);
+
+const itens = separatedData.separed();
+
+console.log(itens);
